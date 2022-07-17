cca9ef67691b5e1afaff934cdbcf2f18ae0cdd80
lmaupas <louis.maupas@framatome.com>
2022-07-17 21:46:35 +0200

    fix:

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import ModalApp from "./lib/ModalApp";
 
 function App() {
   return (
-    <>
-      {/* On a un seul composant pour le bouton et la modal.
-          Le bouton prends en props tous les paramètres utiles pour afficher et configurer la modal et ce bouton */}
-      <ModalApp
-        buttonStyle="CSS du bouton"
-        buttonText="Texte du bouton"
-        modalMessage="Texte du modal"
-      />
-    </>
+    <ModalApp
+      buttonStyle="CSS du bouton"
+      buttonText="Texte du bouton"
+      modalMessage="Texte du modal"
+    />
   );
 }
 
